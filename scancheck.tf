243061521540e4932597e6f14271dcadb04798b5
iactest4zscaler <94528866+iactest4zscaler@users.noreply.github.com>
2022-03-30 11:05:34 +0530

    Update scancheck.tf

--- scancheck.tf
+++ scancheck.tf
@@ -97,7 +97,7 @@ resource "aws_route_table_association" "km_private_route_table_association" {
 }
 
 ### Security
-
+#fsalfe
 # ALB Security group
 # This is the group you need to edit if you want to restrict access to your application
 resource "aws_security_group" "km_alb_sg" {
